8baa5bdc11ea646f8180e17ef0ba0a8d4f49efe8
jongmee <ra123221@gmail.com>
2023-09-19 17:03:45 +0900

    :sparkles: 간단한 Controller 생성하기

---
+++ src/main/java/com/ceos18/springboot/GardenController.java
@@ -0,0 +1,14 @@
+package com.ceos18.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GardenController {
+
+  @GetMapping("/")
+  public String index() {
+    return "Welcome to My Garden in Spring 🌷";
+  }
+
+}
