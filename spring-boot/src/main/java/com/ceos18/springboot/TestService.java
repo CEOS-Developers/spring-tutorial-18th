95268aab4f0dd60653a75423f222517a464b31ea
younggyo <lee4519698@hanmail.net>
2023-09-22 16:27:36 +0900

    Feat : DB 연결 테스트

---
+++ spring-boot/src/main/java/com/ceos18/springboot/TestService.java
@@ -0,0 +1,18 @@
+package com.ceos18.springboot;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TestService {
+    private final TestRepository testRepository;
+
+    @Transactional(readOnly = true)
+    public List<Test> fetchAllTests() {
+        return testRepository.findAll();
+    }
+}
