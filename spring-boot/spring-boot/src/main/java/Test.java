7adc032b37403eda0a0d0a14f9a8cd25190b719b
yunjeong <stac_cato@naver.com>
2023-09-22 20:19:14 +0900

    feat: test Domain, Repository, Service, Controller 작성

---
+++ spring-boot/spring-boot/src/main/java/Test.java
@@ -0,0 +1,12 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Test {
+
+    @Id
+    private Long id;
+    private String name;
+}
---
+++ spring-boot/spring-boot/src/main/java/TestRepository.java
@@ -0,0 +1,3 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestRepository extends JpaRepository<Test, Long> {}
