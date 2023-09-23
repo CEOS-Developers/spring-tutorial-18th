8b46aba48316698d3e84013b1ce700e77665d405
yunjeong <stac_cato@naver.com>
2023-09-23 14:00:57 +0900

    docs: 어노테이션과 테스트 개념 추가

--- spring-boot/spring-boot/src/main/java/com/ceos18/springboot/Test.java
+++ spring-boot/spring-boot/src/main/java/com/ceos18/springboot/Test.java
@@ -3,6 +3,7 @@ package com.ceos18.springboot;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
+import org.springframework.context.annotation.ComponentScan;
 
 @Data
 @Entity
