0d77f8755408b36a9ef340463c965f016e54f797
yeni-choi <yewonchoi@ewhain.net>
2023-10-07 02:02:17 +0900

    update: modify Wishlist Entity structure

--- src/main/java/com/ceos18/springboot/post/entity/Wishlist.java
+++ src/main/java/com/ceos18/springboot/post/entity/Wishlist.java
@@ -9,23 +9,17 @@ import java.sql.Timestamp;
 public class Wishlist {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "wishlistId")
-    private int wishlistId;
+    private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "userId", nullable = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
     private User user;
 
     @ManyToOne
-    @JoinColumn(name = "postId", referencedColumnName = "postId", nullable = false)
+    @JoinColumn(name = "post_id", referencedColumnName = "id", nullable = false)
     private Post post;
 
-    @Column(name = "created", nullable = false)
-    private Timestamp created;
-
-    @Column(name = "updated", nullable = false)
-    private Timestamp updated;
-
+    // TODO: PostStauts로 변경
     @Column(name = "status", nullable = false, length = 10)
     private String status;
 
